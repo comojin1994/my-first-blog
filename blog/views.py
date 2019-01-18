cdce7179180ec329908c69bd16c7eeda031ad052
Your Name <you@example.com>
2019-01-18 20:51:40 +0900

    revise

--- blog/views.py
+++ blog/views.py
@@ -1,17 +1,16 @@
+from django.shortcuts import redirect
 from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
 from .forms import PostForm
-from django.shortcuts import redirect
-
 
 def post_list(request):
     posts = Post.objects.order_by('published_date')
-    return render(request, 'blog/post_list.html', {'posts': posts})
+    return render(request, 'blog/post_list.html', {'posts':posts})
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
-    return render(request, 'blog/post_detail.html', {'post': post})
+    return render(request, 'blog/post_detail.html', {'post':post})
 
 def post_new(request):
     if request.method == "POST":
@@ -39,3 +38,6 @@ def post_edit(request, pk):
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
+
+def test(request):
+    return render(request, 'blog/test.html')
--- mysite/settings.py
+++ mysite/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'l3oygu2irr&=$&29tam&v(5+rg=*w(scfns=0*am8b6vjf*-yd'
+SECRET_KEY = '&j0x@obhkxbo-+w6r3o5#tj$cgq#rclru2ai%ljr%_%pqut1s2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
